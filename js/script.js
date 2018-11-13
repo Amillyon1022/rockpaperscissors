890b666e831fee7c677c56cb855e4da3790c501e
Amillyon Cannon <amillyoncanoon@gmail.com>
2018-11-13 23:01:27 +0000

    progress

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+$("#shoot").click(function(){
+$("#userChoice").text($("#input").val());
+
+var randomNumber= Math.random();
+
+if(randomNumber<.333){
+    $("#computerChoice").html("rock")
+}
+
+else if(randomNumber>.333){
+    $("#computerChoice").html("paper")
+}
+
+});
+
+
+
+
 
 
 
